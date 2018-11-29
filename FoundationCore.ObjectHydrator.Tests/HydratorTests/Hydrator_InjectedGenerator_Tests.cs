3cd1da9ef827c9cdb997451a121c9dc5d84b1a6d
Ryan Smith <ryans@ocgoodwill.org>
2018-11-28 21:55:56 -0800

    Hydrator Injected Generator Tests passing

---
+++ FoundationCore.ObjectHydrator.Tests/HydratorTests/Hydrator_InjectedGenerator_Tests.cs
@@ -0,0 +1,21 @@
+﻿using FoundationCore.ObjectHydrator.Tests.POCOs;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoundationCore.ObjectHydrator.Tests.HydratorTests
+{
+    [TestFixture]
+    class Hydrator_InjectedGenerator_Test
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            var hydrator = new Hydrator<Address>().WithCustomGenerator(x => x.State, new InjectedGenerator());
+
+            var checkme = hydrator.GetSingle();
+            Assert.IsNotNull(checkme);
+        }
+    }
+}
