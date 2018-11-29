0817d1a5131124f247d6df01e2a022ab130eb72a
Ryan Smith <ryans@ocgoodwill.org>
2018-11-28 22:03:10 -0800

    Add simple console test harness

---
+++ FoundationCore.ObjectHydrator.ConsoleTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoundationCore.ObjectHydrator.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var hydrator = new Hydrator<SimpleCustomer>();
+            var customers = hydrator.GetList(5);
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
+            Console.ReadLine();
+        }
+    }
+}
