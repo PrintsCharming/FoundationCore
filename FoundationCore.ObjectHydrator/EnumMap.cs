32335b461cc563b5a0e4880b8e3c206a7e62a61f
Ryan Smith <ryans@ocgoodwill.org>
2018-11-28 21:38:51 -0800

    Simple Address Hydrator Passing

---
+++ FoundationCore.ObjectHydrator/EnumMap.cs
@@ -0,0 +1,32 @@
+﻿using FoundationCore.ObjectHydrator.Generators;
+using FoundationCore.ObjectHydrator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace FoundationCore.ObjectHydrator
+{
+    public class EnumMap : IMap
+    {
+
+        #region IMap Members
+
+        Type IMap.Type
+        {
+            get { return typeof(object); }
+        }
+
+        bool IMap.Match(PropertyInfo info)
+        {
+            return info.PropertyType.IsEnum;
+        }
+
+        IMapping IMap.Mapping(PropertyInfo info)
+        {
+            return new Mapping<object>(info, new EnumGenerator(Enum.GetValues(info.PropertyType)));
+        }
+
+        #endregion
+    }
+}
